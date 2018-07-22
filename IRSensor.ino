8d19ada60dcbe5079ecb44d33c99c2568a6e47c0
Priyal2807 <priyalsobti@gmail.com>
2018-07-22 11:41:10 +0530

    IR Sensor code

---
+++ IRSensor.ino
@@ -0,0 +1,23 @@
+int led=13;
+int obsPin=7;
+int obs=LOW;
+
+void setup() {
+ pinMode(led,OUTPUT);
+ pinMode(obsPin,OUTPUT);
+ Serial.begin(9600); 
+}
+
+void loop() {
+ obs=digitalRead(obsPin);
+ if(obs==HIGH){
+  Serial.println("Object detected");
+  digitalWrite(led,HIGH);
+ }
+
+ else{
+  Serial.println("No Object");
+  digitalWrite(led,LOW);
+ }
+
+}
